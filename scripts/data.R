4b5b08e5abd49ea8897c5321138c20c62e80fe51
mattkielar <matthewkielar16@gmail.com>
2026-02-06 17:58:44 -0800

    create wrapper function to pull FRED data

--- scripts/data.R
+++ scripts/data.R
@@ -1,27 +1,48 @@
 library(dplyr)
 library(tidyr)
 library(purrr)
-library(lubridate)
-library(xts)
 library(fredr)
-library(cansim)
+library(docstring)
+library(vars)
 
 ## --- Pulling data for the USA ---
 
-us_params <- list(
-    series_id = c("INDPRO", # industrial production
-                  "UNRATE", # unemployment rate
-                  "FEDFUNDS", # federal funds rate
-                  "PCE", # CPI for urban consumers
-                  "PCETRIM12M159SFRBDAL", # trimmed mean PCE
-                  "WTISPLC", # WTI crude oil spot price
-                  "RTWEXBGS"), # broad US dollar index
-    frequency = c("m")
-)
+get_fred_data <- function(series, freq = c("m"), start, end) {
+    #' Pull data from FRED
+    #' 
+    #' Calls FRED to pull data on specified series with custom time windows
+    #' 
+    #' @param series A character vector of IDs corresponding to a FRED series
+    #' @param freq One-element vector specifying data frequency, monthly by default
+    #' @param start Start date for the filtered series
+    #' @param end End date for the filtered series
+    #' @return A filtered dataframe
+    #' @examples 
+    #' get_fred_data(c("FEDFUNDS"),"2007-01-01", "2010-01-01")
 
-us_var_data <- purrr::pmap_dfr(
-    .l = us_params,
-    .f = ~ fredr(series_id = .x, frequency = .y)
-)
+    # get info for FRED data series
+    params <- list(
+        series_id = series,
+        frequency = freq)
 
-write.csv(us_var_data, "us_var_data.csv")
+    # call data from FRED
+    data <- purrr::pmap_dfr(
+        .l = params,
+        .f = ~ fredr(series_id = .x, frequency = .y))   
+
+    # filter data to match specified windows
+    data_clean <- data %>%
+        dplyr::select(
+            date, series_id, value) %>%
+        filter(
+            date >= as_date(start) & 
+            date <= as_date(end)) %>%
+        pivot_wider(
+            names_from = series_id,
+            values_from = value)
+    
+    return(data_clean)
+}
+
+us_var_data <- get_fred_data(series = series, "1985-01-01", "2019-01-01")
+
--- scripts/var.R
+++ scripts/var.R
@@ -1,2 +1,10 @@
 library(vars)
 library(BVAR)
+
+source("scripts\\data.R")
+
+series = c("FEDFUNDS", "UNRATE", "PCE", "INDPRO", "WTISPLC", "RTWEXBGS")
+
+us_var_data <- get_fred_data(
+    series = series, start = "1985-01-01", end = "2019-01-01"
+)
