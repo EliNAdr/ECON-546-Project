5975960bd269b60af6949af77fa504c5515afb76
Matthew Kielar <matthewkielar@mattkielar.local>
2026-02-12 22:33:58 -0800

    add function to automate stationarity checks

--- scripts/data.R
+++ scripts/data.R
@@ -1,9 +1,9 @@
 library(dplyr)
 library(tidyr)
 library(purrr)
+library(lubridate)
 library(fredr)
 library(docstring)
-library(vars)
 
 ## --- Pulling data for the USA ---
 
@@ -12,7 +12,8 @@ get_fred_data <- function(series, freq = c("m"), start, end) {
     #' 
     #' Calls FRED to pull data on specified series with custom time windows
     #' 
-    #' @param series A character vector of IDs corresponding to a FRED series
+    #' @param series A character vector of IDs corresponding to a FRED series. If using Cholesky identification,
+    #' series must be arranged in order of decreasing exogeneity.
     #' @param freq One-element vector specifying data frequency, monthly by default
     #' @param start Start date for the filtered series
     #' @param end End date for the filtered series
@@ -21,9 +22,7 @@ get_fred_data <- function(series, freq = c("m"), start, end) {
     #' get_fred_data(c("FEDFUNDS"),"2007-01-01", "2010-01-01")
 
     # get info for FRED data series
-    params <- list(
-        series_id = series,
-        frequency = freq)
+    params <- list(series_id = series, frequency = freq)
 
     # call data from FRED
     data <- purrr::pmap_dfr(
@@ -44,5 +43,34 @@ get_fred_data <- function(series, freq = c("m"), start, end) {
     return(data_clean)
 }
 
-us_var_data <- get_fred_data(series = series, "1985-01-01", "2019-01-01")
+stationarity_check <- function(df, lag = 1, order = 1) {
+    #' Automated stationarity checks for time-series data
+    #' 
+    #' Checks dataframe containing time-series variables for stationarity via an ADF test
+    #' and applies first-differencing for nonstationary variables
+    #' 
+    #' @param df A dataframe containing time-series data
+    #' @param lag The lag to use if applying differencing
+    #' @param diff The order of difference to use 
+    #' @return A filtered dataframe
+    #' @examples
+    #' stationarity_check(var_data, lag = 1, diff = 1)
+    
+    # preliminary checks
+    if (is.data.frame(df) != TRUE) {
+        stop("df must be a dataframe")
+    }
 
+    # check columns iteratively for stationarity
+    cols <- colnames(df[, -1])
+    for (col in cols) {
+        res <- tseries::adf.test(df[[col]])
+        if (res$p.value >= 0.05) {
+            df[[col]] <- collapse::fdiff(df[[col]], n = lag, diff = order, fill = NA)
+        } else {
+            df[[col]] <- df[[col]]
+        }
+    }
+    df <- na.omit(df) # drop first NA row
+    return(df)
+}
--- scripts/var.R
+++ scripts/var.R
@@ -1,10 +1,10 @@
-library(vars)
-library(BVAR)
+source("./scripts/data.R")
 
-source("scripts\\data.R")
+series <- c("INDPRO", "UNRATE", "MEDCPIM158SFRBCLE", "FEDFUNDS", "WTISPLC")
+us_var_data <- get_fred_data(series = series, start = "1985-01-01", end = "2019-01-01")
+us_var_data_stn <- stationarity_check(us_var_data, lag = 1, order = 1)
 
-series = c("FEDFUNDS", "UNRATE", "PCE", "INDPRO", "WTISPLC", "RTWEXBGS")
+lag_selection <- VARselect(us_var_data[, c(2:5)], lag.max = 10)
+var_mod <- VAR(us_var_data_stn[, c(2:5)], p = 6)
+irf <- irf(var_mod, ortho = TRUE, n.ahead = 12) %>% plot()
 
-us_var_data <- get_fred_data(
-    series = series, start = "1985-01-01", end = "2019-01-01"
-)
