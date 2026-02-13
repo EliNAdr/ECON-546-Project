2a5c63c69b41ce3c0ac43e194c70fc8436b09656
EliNAdr <eli.nikolai.adrian@gmail.com>
2026-02-13 00:35:43 -0800

    work on Canada VAR

--- Canada_code/Canada_VAR_baseline.R
+++ Canada_code/Canada_VAR_baseline.R
@@ -2,11 +2,33 @@
 library(vars)
 library(BVAR)
 library(here)
+library(tseries)
 
 # loading data
 rate <- read.csv(here("Canada_data", "BOC_rate.csv"))
-unemp <- read.csv(here("Canada_data", "Can_unemployment.csv"))
+unemp <- read.csv(here("Canada_data", "unemployment.csv"))
 inf <- read.csv(here("Canada_data", "CPI.csv"))
 GDP <- read.csv(here("Canada_data", "rGDP.csv"))
 
-print(head(GDP))
+# merging data, selecting variables, converting to time series, and first differencing for stationarity
+merged_data <- merge(rate, unemp, by = "date")
+merged_data <- merge(merged_data, inf, by = "date")
+merged_data <- merge(merged_data, GDP, by = "date")
+
+merged_data$log_real_GDP <- log(merged_data$real_GDP)
+
+basic_data <- merged_data[c("overnight_rate","unemp_rate","log_real_GDP","CPI_MEDIAN")]
+
+basic_ts <- ts(basic_data, start = c(1995, 1), end = c(2020, 1), frequency = 12)
+
+basic_ts <- diff(basic_ts)
+
+# checking for stationarity - all stationary
+#print(adf.test(basic_ts[,"overnight_rate"]))
+#print(adf.test(basic_ts[,"unemp_rate"]))
+#print(adf.test(basic_ts[,"log_real_GDP"]))
+#print(adf.test(basic_ts[,"CPI_MEDIAN"]))
+
+# var model
+var_model <- VAR(basic_ts, ic = "AIC", lag.max = 12)
+print(var_model)
