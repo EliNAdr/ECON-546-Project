b700893a613a1edff308401b5e3741898ca14e56
mattkielar <matthewkielar16@gmail.com>
2026-02-13 18:50:43 -0800

    add wrapper function to automate VAR and IRF estimation

--- scripts/data.R
+++ scripts/data.R
@@ -1,14 +1,12 @@
-library(dplyr)
-library(tidyr)
-library(purrr)
-library(lubridate)
 library(fredr)
 library(docstring)
 
-## --- Pulling data for the USA ---
+# set API key for FRED
+fredr_set_key("243ab59b7e8b76bd8bd9cd4c53e694d3")
 
+## --- Pulling data for the USA ---
 get_fred_data <- function(series, freq = c("m"), start, end) {
-    #' Pull data from FRED
+    #' Wrapper function to pull data from FRED
     #' 
     #' Calls FRED to pull data on specified series with custom time windows
     #' 
@@ -30,13 +28,13 @@ get_fred_data <- function(series, freq = c("m"), start, end) {
         .f = ~ fredr(series_id = .x, frequency = .y))   
 
     # filter data to match specified windows
-    data_clean <- data %>%
+    data_clean <- data |>
         dplyr::select(
-            date, series_id, value) %>%
-        filter(
-            date >= as_date(start) & 
-            date <= as_date(end)) %>%
-        pivot_wider(
+            date, series_id, value) |>
+        dplyr::filter(
+            date >= lubridate::as_date(start) & 
+            date <= lubridate::as_date(end)) |>
+        tidyr::pivot_wider(
             names_from = series_id,
             values_from = value)
     
@@ -56,7 +54,7 @@ stationarity_check <- function(df, lag = 1, order = 1) {
     #' @examples
     #' stationarity_check(var_data, lag = 1, diff = 1)
     
-    # preliminary checks
+    # preliminary check
     if (is.data.frame(df) != TRUE) {
         stop("df must be a dataframe")
     }
--- scripts/var.R
+++ scripts/var.R
@@ -1,10 +1,63 @@
+library(vars)
 source("./scripts/data.R")
 
 series <- c("INDPRO", "UNRATE", "MEDCPIM158SFRBCLE", "FEDFUNDS", "WTISPLC")
 us_var_data <- get_fred_data(series = series, start = "1985-01-01", end = "2019-01-01")
 us_var_data_stn <- stationarity_check(us_var_data, lag = 1, order = 1)
 
-lag_selection <- VARselect(us_var_data[, c(2:5)], lag.max = 10)
-var_mod <- VAR(us_var_data_stn[, c(2:5)], p = 6)
-irf <- irf(var_mod, ortho = TRUE, n.ahead = 12) %>% plot()
+get_var_irf <- function(y_t, p_max, ortho = FALSE, k_steps, 
+                        plot = FALSE, which_var = NULL) {
+    #' Wrapper function to fit a VAR model with automatic lag selection
+    #' 
+    #' Fits a VAR model for the supplied matrix Y_t with lag length automatically determined 
+    #' via AIC and returns impulse response functions
+    #' 
+    #' @param y_t Dataframe containing endogenous variables for the VAR
+    #' @param p_max Maximum number of lags to consider for lag selection
+    #' @param ortho Whether to compute orthogonalised shocks. FALSE by default.
+    #' @param k_steps Time horizon k for the impulse responses
+    #' @param plot Return plots of computed impulse responses?
+    #' @param which_var Variable to return impulse responses for
+    #' @return A list containing criteria for lag selection, fitted coefficients, and impulse responses
+    #' @examples
+    #' get_var_irf(y_t = us_var_data, max_lag = 10, k_steps = 12)
+    
+    # preliminary check
+    if (is.data.frame(y_t) != TRUE) {
+        stop("y_t must be a data frame")
+    }
 
+    # run lag selection via AIC
+    lagselect <- VARselect(y_t, lag.max = p_max)
+    optim_lag <- lagselect$selection[1]
+
+    # fit VAR with optimal lag length
+    mod <- VAR(y_t, p = optim_lag)
+    irf <- irf(mod, ortho = ortho, n.ahead = k_steps)$irf
+
+    # collect results
+    out <- list(
+        lag_selection = lagselect$criteria,
+        var_coefs = mod$varresults,
+        irf = irf)
+    
+    if (plot == TRUE) {
+        irf_data <- irf[[which_var]] |> 
+            as.data.frame() |>
+            pivot_longer(cols = c(colnames(y_t)),
+                names_to = "variable",
+                values_to = "value") |>
+            dplyr::mutate(t = rep(c(1:(k_steps+1)), length(colnames(y_t))))
+        
+        irf_plot <- ggplot2::ggplot(
+            data = irf_data, aes(x = t, y = value)) +
+            geom_line(linewidth = 1.2, color = "black") + 
+            facet_wrap(~ variable)
+        
+        out$plot <- irf_plot
+    }
+    return(out)
+}
+
+test <- get_var_irf(y_t = us_var_data[, 2:5], p_max = 10, k_steps = 12, 
+                    ortho = TRUE, plot = TRUE, which_var = "FEDFUNDS")
