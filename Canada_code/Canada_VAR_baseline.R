779d8629227ce1c7b6090517b81fb64b2337a536
EliNAdr <eli.nikolai.adrian@gmail.com>
2026-02-15 12:13:46 -0800

    Finalized Canada baseline

--- Canada_code/Canada_VAR_baseline.R
+++ Canada_code/Canada_VAR_baseline.R
@@ -14,27 +14,21 @@ GDP <- read.csv(here("Canada_data", "rGDP.csv"))
 GDP$date <- as.Date(paste0("01 ", GDP$date), format = "%d %B %Y")
 GDP$rGDP <- as.numeric(gsub("[^0-9.-]", "", GDP$Dollars))
 
-# merging data, selecting variables, converting to time series, and first differencing for stationarity
+# merging data, selecting variables, converting to time series, and first differencing log rGDP
 merged_data <- merge(rate, unemp, by = "date")
 merged_data <- merge(merged_data, inf, by = "date")
 merged_data <- merge(merged_data, GDP, by = "date")
 
 merged_data$log_real_GDP <- log(merged_data$rGDP)
-
 basic_data <- merged_data[c("log_real_GDP","CPI_MEDIAN", "unemp_rate", "overnight_rate")]
 
+basic_data$log_real_GDP <- c(NA, diff(basic_data$log_real_GDP))
+
 basic_ts <- ts(basic_data, start = c(1997, 1), end = c(2020, 1), frequency = 12)
-
-basic_ts <- diff(basic_ts)
-
-# checking for stationarity - all stationary at p < 0.01 once differenced
-#print(adf.test(basic_ts[,"overnight_rate"]))
-#print(adf.test(basic_ts[,"unemp_rate"]))
-#print(adf.test(basic_ts[,"log_real_GDP"]))
-#print(adf.test(basic_ts[,"CPI_MEDIAN"]))
+basic_ts <-  na.omit(basic_ts)
 
 # var model & IRFs
-var_model <- VAR(basic_ts, p = 12, type = "const")
+var_model <- VAR(basic_ts, type = "const", p = 12)
 #print(var_model)
 irf_results <- get_var_irf(var_model, shock = "overnight_rate", resp = c("log_real_GDP", "CPI_MEDIAN", "unemp_rate", "overnight_rate"), ortho = TRUE, horizon = 36, plot = TRUE)
 print(irf_results$irf_chart)
