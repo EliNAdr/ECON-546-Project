06c17d91519e4605ea15d07bd9e0f43e0391b5a2
EliNAdr <eli.nikolai.adrian@gmail.com>
2026-02-14 18:00:53 -0800

    Canada initial IRFs

--- Canada_code/Canada_VAR_baseline.R
+++ Canada_code/Canada_VAR_baseline.R
@@ -1,9 +1,9 @@
 # libraries
 library(vars)
-library(BVAR)
 library(here)
 library(tseries)
 library(dplyr)
+library(ggplot2)
 
 # loading data
 rate <- read.csv(here("Canada_data", "BOC_rate.csv"))
@@ -22,7 +22,7 @@ basic_data <- merged_data[c("overnight_rate","unemp_rate","log_real_GDP","CPI_ME
 
 basic_ts <- ts(basic_data, start = c(1995, 1), end = c(2020, 1), frequency = 12)
 
-basic_ts <- diff(basic_ts)
+basic_ts <- diff(diff(basic_ts))
 
 # checking for stationarity - all stationary
 #print(adf.test(basic_ts[,"overnight_rate"]))
@@ -30,4 +30,11 @@ basic_ts <- diff(basic_ts)
 #print(adf.test(basic_ts[,"log_real_GDP"]))
 #print(adf.test(basic_ts[,"CPI_MEDIAN"]))
 
+# selecting optimal lag number - 25
+#print(VARselect(basic_ts, lag.max = 36, type = "const"))
+
 # var model
+var_model <- VAR(basic_ts, lag.max = 36, ic = "AIC", type = "const")
+print(var_model)
+irf_results <- get_var_irf(var_model, shock = "CPI_MEDIAN", resp = c("overnight_rate", "unemp_rate", "log_real_GDP", "CPI_MEDIAN"), horizon = 36, plot = TRUE)
+print(irf_results$irf_chart)
